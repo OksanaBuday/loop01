f19c426e151abc5ba55154a1d8867feaea009eff
Oksana Buday <oksanabuday@gmail.com>
2017-06-26 18:45:55 +0000

    Initial

---
+++ js/index.js
@@ -0,0 +1,55 @@
+function prn(msg){
+    document.write(msg + '<br>');
+}
+// function isPrime(num){
+//     for (var i=2; i<num;i++ ){
+//         if ((num % i) ==0){
+//             return false;
+//         }
+//     }
+//         return true;
+// }
+
+// // for(var i = 100; i> 0; i--){
+// //     if (isPrime(i)){
+// //     prn(i);
+// // }
+// // }
+// var i = 100;
+// while (i>0){
+//       if (isPrime(i)){
+//     prn(i);
+// }
+// i--;
+// }
+
+// var cars = ['BMV', 'Ferari', 'Zaz'];
+// prn(cars.length);
+// cars[3] = 'Toyota';
+// prn(cars.length);
+// prn(cars[0]);
+
+// cars.push('Audi');
+// prn(cars.length);
+
+// prn('unefisting');
+// prn(cars[10]);
+
+// // for(var i=0; i < cars.length; i++){
+// //     prn(cars[i]);
+// // }
+
+// cars.forEach(function(el){
+//     prn(el)
+// })
+
+// var carYears = {
+//     'Ferari': 2010,
+//     'Zaz':1999,
+//     'Toyota': 2011
+// };
+// carYears ['Bmv'] = 2015;
+//  for( var key in carYears){
+//      prn(key+ ':' + carYears)
+//  }
+
